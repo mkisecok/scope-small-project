f981a530a9a794840a9c08a87489e809c0a9d75e
Mkisecok <mahmutkisecok@gmail.com>
2021-09-27 14:09:44 +0200

    Add task1

---
+++ index.js
@@ -0,0 +1,9 @@
+// Aufgabe 1
+
+const student = {
+    firstName: 'John',
+    lastName: 'Smith',
+    class:12
+};
+let result= `${student.firstName} ${student.lastName} is a student in class ${student.class}`;
+console.log(result);
